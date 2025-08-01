878331957bbeae30888b2fd2b5b599da7083e856
Rain Robert <rbacabis24725@liceo.edu.ph>
2025-08-01 09:47:05 +0800

    activity lab 2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,19 @@
-public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int totalNumber = 0;
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Enter number " + i + ": ");
+            int number = scanner.nextInt();
+            totalNumber += number;
+        }
+
+        System.out.println("\nTotal Number: " + totalNumber);
+
+        scanner.close();
     }
 }
