58dd5d6a937847ad29d575764de7fe3bcbbac44e
Rain Robert <rbacabis24725@liceo.edu.ph>
2025-08-08 10:13:27 +0800

    lab-activity-3

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        book book1 = new book("Ralph the explorer", "Partrick", 500);
+        book book2 = new book("Shote the pirates", "Eunson", 300);
+        book book3 = new book("doniel the korean", "Nicholai", 600);
 
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+        book1.borrowBook();
+        book1.displayInfo();
+
+        book1.returnBook();
+        book1.displayInfo();
     }
 }
