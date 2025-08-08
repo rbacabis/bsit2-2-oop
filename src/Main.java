3bc9f44c870ccfd88c0714dc90be81bbfa218a74
Rain Robert <rbacabis24725@liceo.edu.ph>
2025-08-08 10:04:42 +0800

    prelim-exam

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,32 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
 
+        Student s1 = new Student("Alice", 20, "BSIT", 85, 90, 88);
+        Student s2 = new Student("Bob", 19, "BSCS", 92, 95, 89);
+        Student s3 = new Student("Charlie", 21, "BSIT", 65, 70, 68);
+
+        Student[] list = {s1, s2, s3};
+
+        int passCount = 0;
+
+        System.out.println("\nStudent Information:\n");
+
+        for (Student s : list) {
+            s.showInfo();
+            double avg = s.getAverage();
+            String grade = s.getGrade();
+            boolean pass = s.isPass();
+
+            System.out.printf("Average: %.2f\n", avg);
+            System.out.println("Letter Grade: " + grade);
+            System.out.println("Status: " + (pass ? "PASSING\n" : "FAILING\n"));
+
+
+            if (pass) {
+                passCount++;
+            }
+        }
+
+        System.out.println("Summary: " + passCount + " out of 3 students are passing");
     }
 }
