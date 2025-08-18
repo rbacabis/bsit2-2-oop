e1974d0d88248eb10ebec6b826317181e7dd0dcb
Rain Robert <rbacabis24725@liceo.edu.ph>
2025-08-18 11:28:10 +0800

    lab-activity-4

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
-    public static void main(String[] args){
-        System.out.println("Hello World");
+    public static void main(String[] args) {
+        GradeCalculator calculator = new GradeCalculator();
 
+        double[] grades = {85.5, 92.0, 78.5, 90.0};
+        double average = calculator.calculateAverage(grades);
+        String letterGrade = calculator.getLetterGrade(average);
+
+        calculator.displayResult("John Smith", average);
+        calculator.displayResult("John Smith", average, letterGrade);
     }
 }
